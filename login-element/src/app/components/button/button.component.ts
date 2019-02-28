135e8748627723fa7d638f5d6649051eea8e252c
aashkashah <aashka88@gmail.com>
2019-02-27 23:44:57 -0800

    Add files via upload

---
+++ login-element/src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'button-element',
+    templateUrl: './button.component.html'
+  })
+
+  export class ButtonComponent {
+    @Input()
+   data: Data;
+
+   @Input()
+   text: string;
+
+  }
+
+  interface Data {
+      title: string,
+      description: string
+  }
